83285bc919f8a1afb377e5f42e601749952bf32b
Yon <yonrecio@gmail.com>
2023-04-10 10:14:35 +0200

    Update datos-futbol.js

--- recursos/datos-futbol.js
+++ recursos/datos-futbol.js
@@ -28,15 +28,6 @@ let datos = [
         Goles_a_favor_con_el_pie_derecho: 7,
         Goles_a_favor_de_penalti: 2,
         url_icon: 'https://statics-maker.llt-services.com/cad/images/2023/01/13/xlarge/6081f93a16fe78138f52ef891f18f632.png'
-    },
-    {
-        Equipo: "Betis",
-        Goles_a_favor: 23,
-        Goles_a_favor_de_cabeza: 0,
-        Goles_a_favor_con_el_pie_izquierdo: 10,
-        Goles_a_favor_con_el_pie_derecho: 4,
-        Goles_a_favor_de_penalti: 1,
-        url_icon: 'https://www.realbetisbalompie.es/media/img/graphics/new_logos/logo_horizontal.svg'
     }
 
     ]
